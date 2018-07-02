06e0c7d6d8ff493f33aa3604b32b1bc1130ca648
Sascha Hauke <sascha.hauke@ovsoftware.com>
2018-07-02 09:53:14 +0200

    added custom preloading strategy

--- 56-lazy-loading/app/app.module.ts
+++ 56-lazy-loading/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadingStrategy, Route } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
 import { MailModule } from './mail/mail.module';
 
 import { AppComponent } from './app.component';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+
+export class CustomPreload implements PreloadingStrategy {
+  preload(route: Route, fn: () => Observable<any>): Observable<any> {
+    return route.data && route.data.preload ? fn() : Observable.of(null);
+  }
+}
 
 export const ROUTES: Routes = [
-  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
+  { path: 'dashboard', data: { preload: false }, loadChildren: './dashboard/dashboard.module#DashboardModule' },
   { path: '**', redirectTo: 'mail/folder/inbox' }
 ];
 
@@ -16,11 +24,12 @@ export const ROUTES: Routes = [
   declarations: [
     AppComponent
   ],
+  providers: [CustomPreload],
   imports: [
     BrowserModule,
     HttpModule,
     MailModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: CustomPreload })
   ],
   bootstrap: [
     AppComponent
